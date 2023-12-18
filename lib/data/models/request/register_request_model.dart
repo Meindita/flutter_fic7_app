0845985cb4c04cd782d1338e600f19c368b49dc7
Meindita <109034031+Meindita@users.noreply.github.com>
2023-12-19 00:48:58 +0700

    Update register_request_model.dart

--- lib/data/models/request/register_request_model.dart
+++ lib/data/models/request/register_request_model.dart
@@ -2,9 +2,9 @@ import 'dart:convert';
 
 class RegisterRequestModel {
 //   {
-//     "email":"saiful@gmail.com",
+//     "email":"rino@gmail.com",
 //     "password": "12345678",
-//     "name":"saiful"
+//     "name":"rino"
 // }
 
   final String email;
