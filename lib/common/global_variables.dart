fb581d678adde491d62165e832671a6c84722346
meindita <meinditarino@gmail.com>
2023-12-19 00:21:13 +0700

    meet10

--- lib/common/global_variables.dart
+++ lib/common/global_variables.dart
@@ -1,3 +1,3 @@
 class GlobalVariables {
-  static const baseUrl = 'https://4436-2001-448a-4063-1006-9120-14c3-2c63-80b4.ngrok.io';
+  static const baseUrl = 'https://13dc-113-11-183-223.ngrok.io';
 }
